b7440bccad75f4651c08040bac78728ba77f6bc1
Aman <amanaraj05a@gmail.com>
2021-02-07 11:40:22 +0530

    completed

---
+++ main.py
@@ -0,0 +1,31 @@
+#This program filters even and odd numbers inside a given limit
+#---------------------------
+
+
+#asking the user whether he want to sort even or odd number
+choice=int(input("\nTo sort even number press 1 and to sort odd number press 2:"))
+
+
+#checking the choice of the user
+if(choice==1):
+
+    #asking the user for upper limit
+    upper_limit=int(input("\nEnter an upper limit:"))
+    print("\nThe even numbers smaller than ",upper_limit," is:")
+
+    for i in range(1,upper_limit) :
+        if(i%2==0):
+            print(i)
+
+
+elif(choice==2):
+
+    #asking the user for upper limit
+    upper_limit=int(input("\nEnter an upper limit:"))
+    print("\nThe odd numbers smaller than ",upper_limit," is:")
+    
+    for i in range(1,upper_limit):
+        if(i%2!=0):
+            print(i)
+else:
+    print("\nInvalid Choice")
