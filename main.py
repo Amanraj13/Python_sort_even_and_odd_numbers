5f1a6735727b9165f13a9730c860e4826321911d
Aman <amanaraj05a@gmail.com>
2021-02-07 12:20:21 +0530

    added some cool icons

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #This program filters even and odd numbers inside a given limit
 #---------------------------
 
+print("""\n♪ ♫ ♩ ♬ ♭ ♮ ♯""","Welcome to Even/Odd number Sorter","""♪ ♫ ♩ ♬ ♭ ♮ ♯""")
 
 #asking the user whether he want to sort even or odd number
 choice=int(input("\nTo sort even number press 1 and to sort odd number press 2:"))
@@ -23,9 +24,11 @@ elif(choice==2):
     #asking the user for upper limit
     upper_limit=int(input("\nEnter an upper limit:"))
     print("\nThe odd numbers smaller than ",upper_limit," is:")
-    
+
     for i in range(1,upper_limit):
         if(i%2!=0):
             print(i)
 else:
     print("\nInvalid Choice")
+
+
